3d8d85a71e1412747a0c9455e25ca50cd3e104eb
Addis Casco <addis.casco@utexas.edu>
2018-02-28 11:20:04 -0600

    changed the format of questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,54 @@
 var quiz = [
-    [1, "Who loves Kanye the most?", "Kanye"],
-    [2, "Which Kardashian sister has the ugliest cry face?", "Kim"],
-    [3, "Which of the following is Kim known for the most?", "Selfies"],
-    [4, "Which song did Kanye write about Kim?", "Awesome"],
-    [5, "Who does Kanye compare himself to?", "All of the above"],
+    {
+        question: "Who loves Kanye the most?",
+        answers: {
+            a: "dfd",
+            b: "sdf",
+            c: "Kanye",
+            d: "Kim"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which Kardashian sister has the ugliest cry face?",
+        answers: {
+            a: "dfd",
+            b: "sdf",
+            c: "Kanye",
+            d: "Kim"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which of the following is Kim known for the most?",
+        answers: {
+            a: "Selfies",
+            b: "sdf",
+            c: "Kanye",
+            d: "Kim"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who does Kanye compare himself to?",
+        answers: {
+            a: "dfd",
+            b: "sdf",
+            c: "Kanye",
+            d: "Kim"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which song did Kanye write about Kim?",
+        answers: {
+            a: "dfd",
+            b: "sdf",
+            c: "Kanye",
+            d: "Kim"
+        },
+        correctAnswer: "c"
+    }
 ];
 
-var answer;
-var response;
-var correct = 0;
-var wrong = 0;
-
-for (var i = 0; i < quiz.length; i++){
-
-    answer = prompt(quiz[i][1]);
-    //storing answer to response
-    response = answer.toLowerCase();
-    //
-    if (response===quiz[i][2]) {
-        document.write(`<h2>You got question ${quiz[i][0]} correct </h2>`);
-        correct++;
-    }
-    
-    else{
-        document.write(`<h2>You got question ${quiz[i][0]} wrong </h2>`);
-        wrong++;
-        console.log(wrong);
-    }
-
-}
 
---
+++ assets/javascript/fizzBuzz.js
@@ -0,0 +1,13 @@
+
+
+var array = [];
+
+function generateNumber (min, max) {
+  var array =Math.floor(Math.random()*(max-min+1))+min
+}
+
+console.log(generateNumber());
+
+// for (var i = 0; i < array.length; i++) {
+
+// }
